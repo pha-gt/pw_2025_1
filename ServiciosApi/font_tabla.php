b6d5c9ce1c5c178f82ff731ffa94bef7d4be0adf
Fabian <fgtfabian@gmail.com>
2025-03-13 13:51:44 -0600

    Tablas en php con invocacion a servicio

--- ServiciosApi/font_tabla.php
+++ ServiciosApi/font_tabla.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html >
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     </head>
 <body>
+    <h1>HOLA TABLA</h1>
+
     <?php
 
     /******  INVOCAR UN SERVICIO *****/
-    $url = "http://localhost/pw_2025_1/php/codigos/objetos2.php";
+    $url = "http://localhost/pw_2025_1/ServiciosApi/API/Auto.php";
     // Inicializa cURL
     $ch = curl_init($url);
     // Configura las opciones de cURL
@@ -18,7 +20,7 @@
         'Content-Type: application/json' //, // Tipo de contenido
         //'Authorization: Bearer tu_token_aqui' // Si es necesario, agrega un token de autorización
     ]);
-    
+
     // Ejecuta la solicitud
     $response = curl_exec($ch);
     // Maneja errores
@@ -27,14 +29,53 @@
     }
     // Cierra la sesión de cURL
     curl_close($ch);
-    
+
     // Procesa la respuesta
     $lista_vehiculos = json_decode($response);
     /*****   FIN DE INVOCA UN SERVICIO *******/
 
 
-    //print_r($data); // Muestra la respuesta
+    //print_r($lista_vehiculos); // Muestra la respuesta
     ?>
+
+    
+    <table class="table">
+    <thead>
+        <tr>
+        <th scope="col">Marca</th>
+        <th scope="col">Pais</th>
+        <th scope="col">Modelo</th>
+        <th scope="col">Año</th>
+        <th scope="col">Motor</th>
+        <th scope="col">Cilintros</th>
+        <th scope="col">Capacidad</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    <?php
+
+    foreach($lista_vehiculos as $vehiculo)
+    {
+        echo "<tr>
+        <th scope='row'>".$vehiculo->Marca->Nombre."</th>
+        <td>".$vehiculo->Marca->Pais."</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        </tr>";
+    }
+
+   
+
+    ?>
+        
+
+
+
+    </tbody>
+    </table>
+
+   
 
 
     
